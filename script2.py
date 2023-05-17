2a70c75b2f5b6889162dd15cbef260a7c870a62a
mrunalpatel2027 <133611286+mrunalpatel2027@users.noreply.github.com>
2023-05-17 15:32:21 -0400

    Put comment back in script2.py
    Comment was unintentionally deleted by revert.

--- script2.py
+++ script2.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
+
 # This is good programming advice
-=======
-# Print file from Zen of Python
->>>>>>> parent of d0aafa7 (Revert "Modified both script files")
 print("Namespaces are one honking great idea -- let's do more of those!")
 print("If the implementation is hard to explain, it's a bad idea.")
