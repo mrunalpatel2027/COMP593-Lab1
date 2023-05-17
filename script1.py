c410a725d5aca856854327db301669f87cfa4fa9
mrunalpatel2027 <133611286+mrunalpatel2027@users.noreply.github.com>
2023-05-17 15:08:17 -0400

    Created script file
    Added code that prints line from Zen of Python

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Special cases aren't special enough to break the rules.")
+print("Complex is better than complicated.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("Namespaces are one honking great idea -- let's do more of those!")
+print("If the implementation is hard to explain, it's a bad idea.")
