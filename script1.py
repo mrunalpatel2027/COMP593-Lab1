f198524c04fce33af564f36ffe4ede67af3de24c
mrunalpatel2027 <133611286+mrunalpatel2027@users.noreply.github.com>
2023-05-17 15:42:44 -0400

    Modified both files in repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statement to scripot2.py

--- script1.py
+++ script1.py
@@ -1,2 +1,2 @@
 # Print file from Zen of Python
-print("Special cases aren't special enough to break the rules.")
+
--- script2.py
+++ script2.py
@@ -2,3 +2,4 @@
 # This is good programming advice
 print("Namespaces are one honking great idea -- let's do more of those!")
 print("If the implementation is hard to explain, it's a bad idea.")
+print("Namespace are on honking great idea.")
