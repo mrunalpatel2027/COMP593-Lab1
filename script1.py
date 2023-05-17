406dc19fe428055cfb4839fe98f3ea09a0dbd231
mrunalpatel2027 <133611286+mrunalpatel2027@users.noreply.github.com>
2023-05-17 15:30:18 -0400

    Revert "Modified both script files"

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print file from Zen of Python
 print("Special cases aren't special enough to break the rules.")
-print("Complex is better than complicated.")
--- script2.py
+++ script2.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 # This is good programming advice
+=======
+# Print file from Zen of Python
+>>>>>>> parent of d0aafa7 (Revert "Modified both script files")
 print("Namespaces are one honking great idea -- let's do more of those!")
 print("If the implementation is hard to explain, it's a bad idea.")
